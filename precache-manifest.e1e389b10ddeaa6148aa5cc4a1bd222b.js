0bf815d7cbc04e8e90df6f20684cb2d1e8729f45
swess15 <swess15@gmail.com>
2019-02-22 12:47:11 +0900

    Updates

---
+++ precache-manifest.e1e389b10ddeaa6148aa5cc4a1bd222b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "49024fe346c716daa83f642f846c72fd",
+    "url": "/test02/static/media/letter.49024fe3.png"
+  },
+  {
+    "revision": "2cb4b3a785acf1b130c9297a184e37a9",
+    "url": "/test02/static/media/Moon_2.2cb4b3a7.png"
+  },
+  {
+    "revision": "c10ec3fcd65ea0fad621e63ee27d7c1e",
+    "url": "/test02/static/media/BackGround_2.c10ec3fc.png"
+  },
+  {
+    "revision": "edff178e87125753ae94",
+    "url": "/test02/static/js/runtime~main.edff178e.js"
+  },
+  {
+    "revision": "58aec5f5343d2366eb70",
+    "url": "/test02/static/js/main.58aec5f5.chunk.js"
+  },
+  {
+    "revision": "7c4d00488b303972d25b",
+    "url": "/test02/static/js/2.7c4d0048.chunk.js"
+  },
+  {
+    "revision": "58aec5f5343d2366eb70",
+    "url": "/test02/static/css/main.b0bf76b7.chunk.css"
+  },
+  {
+    "revision": "1a954b53fd91a1a388abd1573bb1b118",
+    "url": "/test02/index.html"
+  }
+];
